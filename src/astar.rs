4d8f20cd2b67a79abcdb6956820576db1679a581
Xiaomi Zhou <imimo0219@163.com>
2025-07-09 02:09:34 +0800

    astar in progress

--- src/astar.rs
+++ src/astar.rs
@@ -362,21 +362,32 @@ impl AStarModel{
                 let x_max_norm = x_max / *ASTAR_STRIDE;
                 let y_max_norm = y_max / *ASTAR_STRIDE;
                 // to do: check "%"
-                let x_new_clamped = if x_max_norm.floor() == x_max_norm{
+                let x_new_clamped = if new_x == old_x || x_max_norm.floor() == x_max_norm{
                     new_x
                 }else{
-                    x_max_norm.floor()* *ASTAR_STRIDE
+                    if x_max_norm > 0 {
+                        x_max_norm.floor()* *ASTAR_STRIDE
+                    }else{
+                        x_max_norm.ceil()* *ASTAR_STRIDE
+                    }
                 };
-                let y_new_clamped = if y_max_norm.floor() == y_max_norm{
+                let y_new_clamped = if new_y == old_y || y_max_norm.floor() == y_max_norm{
                     new_y
                 }else{
-                    y_max_norm.floor() * *ASTAR_STRIDE
+                    if y_max_norm > 0 {
+                        y_max_norm.floor() * *ASTAR_STRIDE
+                    }else{
+                        y_max_norm.ceil() * *ASTAR_STRIDE
+                    }
                 };
 
                 let x_new_length = (x_new_clamped - old_x).abs();
                 let y_new_length = (y_new_clamped - old_y).abs();
-                let length_bound_by_grid = FixedPoint::min(x_new_length, y_new_length);
-
+                let length_bound_by_grid = FixedPoint::max(x_new_length, y_new_length);
+                if length_bound_by_grid == 0{
+                    println!("old_x:{:?}, x_max_norm: {:?}, x_new_clamped: {:?}", old_x, x_max_norm, x_new_clamped);
+                    println!("old_y:{:?}, y_max_norm: {:?}, y_new_clamped: {:?}", old_y, y_max_norm, y_new_clamped);
+                }
 
                 let end_lines = [
                     (self.end.x, self.end.y, 1.0 , 0.0 ),
@@ -434,49 +445,49 @@ impl AStarModel{
                 }
 
                 let length_bound_by_end_point = min_distance;
-
-                let result_length = FixedPoint::min(length_bound_by_obstacles, length_bound_by_end_point);
+                println!("ob: {:?}, grid: {:?}, end: {:?}",length_bound_by_obstacles, length_bound_by_grid, length_bound_by_end_point);
+                let result_length = FixedPoint::min(FixedPoint::min(length_bound_by_obstacles, length_bound_by_grid), length_bound_by_end_point);
                 try_push_node_to_frontier(result_length); // push the node with the result_length
 
-                if result_length == length_bound_by_end_point{ // todo: put end in frontier
-                    let new_position = get_new_position(result_length);
-                    let new_direction = Direction::from_points(new_position, self.end);
-                    let new_length = FixedPoint::max((self.end.x-new_position.x).abs(), (self.end.y-new_position.y).abs());
-                    let astar_node_key = AstarNodeKey {
-                        position: self.end,
-                    };
-                    // check if the new position is already visited
-                    if !visited.contains(&astar_node_key) {
+                // if result_length == length_bound_by_end_point{ // todo: put end in frontier
+                //     let new_position = get_new_position(result_length);
+                //     let new_direction = Direction::from_points(new_position, self.end);
+                //     let new_length = FixedPoint::max((self.end.x-new_position.x).abs(), (self.end.y-new_position.y).abs());
+                //     let astar_node_key = AstarNodeKey {
+                //         position: self.end,
+                //     };
+                //     // check if the new position is already visited
+                //     if !visited.contains(&astar_node_key) {
                         
-                        // calculate the cost to reach the next node
-                        let turn_penalty = if new_direction == direction {
-                            0.0 // no turn penalty if the direction is the same
-                        } else {
-                            TURN_PENALTY
-                        };
+                //         // calculate the cost to reach the next node
+                //         let turn_penalty = if new_direction == direction {
+                //             0.0 // no turn penalty if the direction is the same
+                //         } else {
+                //             TURN_PENALTY
+                //         };
 
-                        let length: f64 = ((new_direction.to_fixed_vec2().length() * new_length)).to_num();
-                        let actual_cost = current_node.actual_cost + length + turn_penalty;
-                        let actual_length = current_node.actual_length + length;
-                        let estimated_cost = octile_distance(&new_position, &self.end) * ESTIMATE_COEFFICIENT;
-                        let total_cost = actual_cost + estimated_cost;                
-                        let new_node = AstarNode {
-                            position: self.end,
-                            direction: Some(new_direction),
-                            actual_cost,
-                            actual_length,
-                            estimated_cost,
-                            total_cost,
-                            prev_node: Some(current_node.clone()), // link to the previous node
-                        };
-                        // push directly to the frontier
-                        frontier.push(BinaryHeapItem {
-                            key: Reverse(NotNan::new(new_node.total_cost).unwrap()), // use Reverse to make it a min heap
-                            value: Rc::new(new_node),
-                        });
-                    }
+                //         let length: f64 = ((new_direction.to_fixed_vec2().length() * new_length)).to_num();
+                //         let actual_cost = current_node.actual_cost + length + turn_penalty;
+                //         let actual_length = current_node.actual_length + length;
+                //         let estimated_cost = octile_distance(&new_position, &self.end) * ESTIMATE_COEFFICIENT;
+                //         let total_cost = actual_cost + estimated_cost;                
+                //         let new_node = AstarNode {
+                //             position: self.end,
+                //             direction: Some(new_direction),
+                //             actual_cost,
+                //             actual_length,
+                //             estimated_cost,
+                //             total_cost,
+                //             prev_node: Some(current_node.clone()), // link to the previous node
+                //         };
+                //         // push directly to the frontier
+                //         frontier.push(BinaryHeapItem {
+                //             key: Reverse(NotNan::new(new_node.total_cost).unwrap()), // use Reverse to make it a min heap
+                //             value: Rc::new(new_node),
+                //         });
+                //     }
 
-                }
+                // }
 
                 // let midway_length: Option<FixedPoint> = if self.end.x > x_min && self.end.x <x_max{
                 //     Some((self.end.x - old_x).abs())
